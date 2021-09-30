9432cdbb071bb8f1cb60dcdc20f5f2b22228ce6d
Loïc Saint-Roch <loic@nunchi.studio>
2021-09-30 13:42:42 +0200

    fix(docker): Improve build process for platforms
    Signed-off-by: Loïc Saint-Roch <loic@nunchi.studio>

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -2,34 +2,45 @@
 
 for distribution in "alpine" "bullseye" "buster"; do
 	for edition in "standard" "enterprise"; do
-		for os in "linux"; do
-			for arch in "amd64" "arm64"; do
-        if [ $distribution == "bullseye" ] && [ $edition == "standard" ]; then
-          docker buildx build --no-cache \
-						--file ./$distribution/Dockerfile \
-						--platform $os/$arch \
-						--build-arg BLACKSMITH_EDITION=$edition \
-						--build-arg BLACKSMITH_VERSION=${BLACKSMITH_VERSION} \
-						--build-arg GO_OS=$os \
-						--build-arg GO_ARCH=$arch \
-						--tag nunchistudio/blacksmith:latest \
-						--tag nunchistudio/blacksmith:${BLACKSMITH_VERSION} \
-						--tag nunchistudio/blacksmith-standard:latest \
-						--tag nunchistudio/blacksmith-standard:${BLACKSMITH_VERSION} \
-						. ; \
-				else
-					docker buildx build --no-cache \
-						--file ./$distribution/Dockerfile \
-						--platform $os/$arch \
-						--build-arg BLACKSMITH_EDITION=$edition \
-						--build-arg BLACKSMITH_VERSION=${BLACKSMITH_VERSION} \
-						--build-arg GO_OS=$os \
-						--build-arg GO_ARCH=$arch \
-						--tag nunchistudio/blacksmith-$edition:latest-$distribution \
-						--tag nunchistudio/blacksmith-$edition:${BLACKSMITH_VERSION}-$distribution \
-						. ; \
-				fi
-			done
-		done
+    if [[ $distribution == "bullseye" ]] && [[ $edition == "standard" ]]; then
+      docker buildx build --no-cache \
+        --file ./$distribution/Dockerfile \
+        --platform linux/amd64,linux/arm64 \
+        --build-arg BLACKSMITH_EDITION=$edition \
+        --build-arg BLACKSMITH_VERSION=${BLACKSMITH_VERSION} \
+        --tag nunchistudio/blacksmith \
+        --tag nunchistudio/blacksmith:latest \
+        --tag nunchistudio/blacksmith:latest-$distribution \
+        --tag nunchistudio/blacksmith:${BLACKSMITH_VERSION} \
+        --tag nunchistudio/blacksmith:${BLACKSMITH_VERSION}-$distribution \
+        --tag nunchistudio/blacksmith-standard \
+        --tag nunchistudio/blacksmith-standard:latest \
+        --tag nunchistudio/blacksmith-standard:latest-$distribution \
+        --tag nunchistudio/blacksmith-standard:${BLACKSMITH_VERSION} \
+        --tag nunchistudio/blacksmith-standard:${BLACKSMITH_VERSION}-$distribution \
+        --push . ; \
+    elif [[ $distribution != "bullseye" ]] && [[ $edition == "standard" ]]; then
+      docker buildx build --no-cache \
+        --file ./$distribution/Dockerfile \
+        --platform linux/amd64,linux/arm64 \
+        --build-arg BLACKSMITH_EDITION=$edition \
+        --build-arg BLACKSMITH_VERSION=${BLACKSMITH_VERSION} \
+        --tag nunchistudio/blacksmith:latest-$distribution \
+        --tag nunchistudio/blacksmith:${BLACKSMITH_VERSION} \
+        --tag nunchistudio/blacksmith:${BLACKSMITH_VERSION}-$distribution \
+        --tag nunchistudio/blacksmith-standard:latest-$distribution \
+        --tag nunchistudio/blacksmith-standard:${BLACKSMITH_VERSION} \
+        --tag nunchistudio/blacksmith-standard:${BLACKSMITH_VERSION}-$distribution \
+        . ; \
+    else
+      docker buildx build --no-cache \
+        --file ./$distribution/Dockerfile \
+        --platform linux/amd64,linux/arm64 \
+        --build-arg BLACKSMITH_EDITION=$edition \
+        --build-arg BLACKSMITH_VERSION=${BLACKSMITH_VERSION} \
+        --tag nunchistudio/blacksmith-$edition:latest-$distribution \
+        --tag nunchistudio/blacksmith-$edition:${BLACKSMITH_VERSION}-$distribution \
+        . ; \
+    fi
 	done
 done
